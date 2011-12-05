f504270dadaa5049cd7368c21dc23f00cbbe13e7
Adrian Gaudebert <adrian@gaudebert.fr>
2011-12-05 07:21:52 +0100

    Improving unit tests.

--- tests/test_component-entity-manager.js
+++ tests/test_component-entity-manager.js
@@ -1,32 +1,96 @@
+/**
+ * Test cases for ComponentEntityManager class.
+ *
+ * Requires node.js and it's nodeunit module.
+ * To run those tests: nodeunit test_component-entity-manager.js
+ *
+ * @author Adrian Gaudebert - adrian@gaudebert.fr
+ */
+
 var ComponentEntityManager = require('../component-entity-manager'),
     PositionComponent = require('./components/position');
     UnitComponent = require('./components/unit');
 
-var cem = new ComponentEntityManager();
-cem.c('Position', PositionComponent);
-cem.c('Unit', UnitComponent);
+function prepareManager() {
+    var myGE = new ComponentEntityManager();
+    myGE.c('Position', PositionComponent);
+    myGE.c('Unit', UnitComponent);
+    return myGE;
+}
 
-var pos = cem.e('Unit');
-console.log("pos : " + pos);
-console.log("pos.x : " + pos.x);
+exports['add-component'] = function (test) {
+    // One component case
+    var myGE = new ComponentEntityManager();
+    myGE.c('Position', PositionComponent);
 
-pos.x = 45;
-console.log("pos.x : " + pos.x);
+    test.deepEqual(myGE.getComponentsList(), ['obj', 'Position'], 'Bad list of components');
 
-var pos2 = cem.e('Position');
-console.log("pos2 : " + pos2);
-console.log("pos2.x : " + pos2.x);
-console.log("pos.x : " + pos.x);
+    // Two components case
+    myGE.c('Unit', UnitComponent);
+    test.deepEqual(myGE.getComponentsList(), ['obj', 'Position', 'Unit'], 'Bad list of components');
 
-console.log("pos.isAtOrigin : " + pos.isAtOrigin());
-console.log("pos2 : " + pos2.x + ", " + pos2.y + ", " + pos2.z);
-console.log("pos2.isAtOrigin : " + pos2.isAtOrigin());
+    test.done();
+}
 
-console.log("pos.attack : " + pos.attack);
-console.log("pos2.attack : " + pos2.attack);
+exports['create-entity'] = function (test) {
+    // Preparing
+    var myGE = prepareManager(),
+        pos = myGE.e('Position');
 
-console.log("Position: " + cem.get("Position"));
-console.log("Unit: " + cem.get("Unit"));
-console.log("obj: " + cem.get("obj"));
-console.log("obj Position: " + cem.get("obj Position"));
-console.log("obj Unit: " + cem.get("obj Unit"));
+    // Testing default values
+    test.equal(pos.x, 0, 'Bad value for attribute `x`');
+    test.equal(pos.y, 0, 'Bad value for attribute `y`');
+    test.equal(pos.z, 0, 'Bad value for attribute `z`');
+    test.equal(pos.attack, undefined, 'Attribute `attack` should not exist');
+    test.equal(pos.unknownattribute, undefined, 'Attribute `unknownattribute` should not exist');
+
+    pos.x = 12;
+    test.equal(pos.x, 12, 'Bad value for attribute `x`');
+
+    pos.set('x', 13);
+    test.equal(pos.x, 13, 'Bad value for attribute `x`');
+
+    pos.x++;
+    test.equal(pos.x, 14, 'Bad value for attribute `x`');
+    test.equal(pos.get('x'), 14, 'get() method returns wrong result for attribute `x`');
+
+    // Testing an other entity
+    var unit = myGE.e('Unit');
+
+    test.equal(unit.x, 0, 'Bad value for attribute `x`');
+    test.equal(unit.y, 0, 'Bad value for attribute `y`');
+    test.equal(unit.z, 0, 'Bad value for attribute `z`');
+    test.equal(unit.attack, 10, 'Bad value for attribute: attack');
+    test.equal(unit.speed, 1, 'Bad value for attribute: speed');
+
+    // Testing methods
+    pos.x = 0;
+    test.ok(pos.isAtOrigin(), 'Cannot call method isAtOrigin() of Position entity');
+    test.ok(unit.isAtOrigin(), 'Cannot call method isAtOrigin() of Unit entity');
+    test.ok(unit.canReach(), 'Cannot call method canReach() of Unit entity');
+
+    test.done();
+}
+
+exports['get-entity'] = function (test) {
+    // Preparing
+    var myGE = prepareManager(),
+        pos = myGE.e('Position');
+
+    // Testing with one entity
+    test.deepEqual(myGE.get('Position'), [pos], 'Bad list of entities');
+    //~ test.deepEqual(myGE.get(pos.id), [pos], 'Bad entity'); // Not ready yet
+
+    // Testing with two entities
+    var unit = myGE.e('Unit');
+
+    test.deepEqual(myGE.get('Unit'), [unit], 'Bad list of entities');
+    test.deepEqual(myGE.get('obj Unit'), [unit], 'Bad list of entities');
+    test.deepEqual(myGE.get('obj Position Unit'), [unit], 'Bad list of entities');
+    test.deepEqual(myGE.get('Position'), [pos, unit], 'Bad list of entities');
+    test.deepEqual(myGE.get('obj Position'), [pos, unit], 'Bad list of entities');
+    test.deepEqual(myGE.get('obj'), [pos, unit], 'Bad list of entities');
+    //~ test.deepEqual(myGE.get(unit.id), [unit], 'Bad entity'); // Not ready yet
+
+    test.done();
+}
