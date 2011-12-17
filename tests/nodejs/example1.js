8c3771bf93cd84ff9d00af38ce25bbce54aeed5c
Adrian Gaudebert <adrian@gaudebert.fr>
2011-12-17 03:58:26 +0100

    Improve the example on node.js, better output and added some comments.

--- tests/nodejs/example1.js
+++ tests/nodejs/example1.js
@@ -1,23 +1,58 @@
-var util = require('util'),
-    ComponentEntityManager = require('../../component-entity-manager');
+var cem = require('../../component-entity-manager'),
+    util = require('util');
 
-var myGE = new ComponentEntityManager();
+util.log(util.inspect(cem));
+// Instanciate the Game Engine
+var myGE = new cem.ComponentEntityManager();
 
+// Create a few components that can be used in-game
 var Hero = {
     "life": 100,
     "defense": 100,
-    "attack": 100
+    "attack": 100,
+
+    "hit": function(opponent) {
+        var points = this.attack * 1.1 - opponent.defense;
+
+        if (points > 0) {
+            opponent.life -= Math.round(points);
+        }
+        return this;
+    },
+
+    "specialHit": function(opponent) {
+        return this.hit(opponent);
+    }
 }
 
 var Wizard = {
+    // this component requires the Hero component, and
+    // any entity having a Wizard component will automatically have a Hero one
     "_requires": "Hero",
+
     "life": 80,
-    "mana": 100
+    "mana": 100,
+
+    // "Overwrite" the specialHit method of the Hero component
+    "specialHit": function(opponent) {
+        var points = this.attack * 1.2 - opponent.defense;
+
+        if (this.mana > 20) {
+            points += 20;
+            this.mana -= 20;
+        }
+
+        if (points > 0) {
+            opponent.life -= Math.round(points);
+        }
+        return this;
+    }
 }
 
 var Necromancer = {
     "_requires": "Wizard",
-    "life": 70
+    "life": 70,
+    "attack": 110
 }
 
 var Paladin = {
@@ -25,13 +60,29 @@ var Paladin = {
     "defense": 150
 }
 
+// Add those components to the Game Engine so we can use them
 myGE.addComponent("Hero", Hero)
     .addComponent("Wizard", Wizard)
     .addComponent("Necromancer", Necromancer)
     .addComponent("Paladin", Paladin);
 
+// Create two entities than we can manipulate
 var gerard = myGE.e("Paladin"),
     igor = myGE.e("Necromancer");
 
-console.log("gerard = " + util.inspect(gerard));
-console.log("gerard.life = " + gerard.life);
+// And now play with those entities
+console.log("Before we do anything: ");
+console.log("  Gerard's life is " + gerard.life);
+console.log("  Igor's life is " + igor.life);
+
+gerard.hit(igor);
+
+console.log("\nAfter Gerard hits Igor: ");
+console.log("  Gerard's life is " + gerard.life);
+console.log("  Igor's life is " + igor.life);
+
+igor.specialHit(gerard);
+
+console.log("\nAfter Igor hits Gerard: ");
+console.log("  Gerard's life is " + gerard.life);
+console.log("  Igor's life is " + igor.life);
